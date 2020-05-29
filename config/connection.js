5d28c766306ba3e71ad77bcc39bcfc99ee7ed623
Erika <erika.paige.handley@gmail.com>
2020-05-28 22:07:46 -0700

    built config file

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 const mysql = require('mysql2')
 
 // create connection w/ new mysql connection
-const connection = mysql.createConnection('mysql://root:rootroot1@localhost:3306/burgers_db')
+module.exports = mysql.createConnection('mysql://root:rootroot1@localhost:3306/burgers_db')
 
 // below is connection use once deployed on heroku / JAWS
 // module.exports = mysql.createConnection(process.env.JAWSDB_URL || process.env.LOCAL_URL)
-
-module.exports = connection
