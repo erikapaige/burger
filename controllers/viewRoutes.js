4f9069ed79a32515ed5487e71f8f577c9fd5f3a8
Erika <erika.paige.handley@gmail.com>
2020-05-28 23:27:04 -0700

    views folders and handlebar files updated

---
+++ controllers/viewRoutes.js
@@ -0,0 +1,16 @@
+//bringing in routes from handlebars
+const router = require('express').Router()
+// bring in model, so have access to data
+const { Burger } = require('../models')
+
+router.get('/', (req, res) => {
+  //res.render to make sure localhost working
+  res.render('index')
+  //instead of rendering, get data then have it render
+  Burger.getBurgers(burgers =>{
+    //render index hbs, pass it an object (burger data)
+    res.render('index', { burgers })
+  })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
-// app.set('view engine', '.hbs')
+// middlewear for handelbars
+// changed handlebars to .hbs to be used for extension for handelbar files
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 app.use(require('./controllers/index.js'))
-
 
 app.listen(process.env.PORT, () => console.log('http://localhost:3000'))
