36ce62eec26f87d4dd826d76cfd11aefee97fcb6
Erika <erika.paige.handley@gmail.com>
2020-05-29 23:16:54 -0700

    small change

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // bring in npm package mysql2
 const mysql = require('mysql2')
 
-// connection w/ new mysql connection
+// connection w/ new mysql connection,
 // module.exports = mysql.createConnection('mysql://root:rootroot1@localhost:3306/burgers_db')
 
 // below is connection use once deployed on heroku / JAWS
