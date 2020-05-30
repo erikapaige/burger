0a523964deb51af50c577b831c602956abc9c885
Erika <erika.paige.handley@gmail.com>
2020-05-29 19:05:31 -0700

    changed seed data from true to false

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name, devoured) VALUES ('Buzz Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Crispy Bun', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('All-Amereican Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bigger Better Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bigger Better Burger', false);
