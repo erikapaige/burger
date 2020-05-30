77926ad744dfb8338fe1491804b856cc57a0b5ac
Erika <erika.paige.handley@gmail.com>
2020-05-29 17:40:44 -0700

    css attempt

--- public/app.js
+++ public/app.js
@@ -13,6 +13,7 @@ document.getElementById('addBurger').addEventListener('click', event =>{
   axios.post('/api/burgers', {
     //pass it an object, from user's input
     burger_name: document.getElementById('burger_name').value
+    // devoured: document.getElementById
   })
     // update the burger list on page to reflect added burger
     .then(({ data }) =>{
@@ -20,7 +21,7 @@ document.getElementById('addBurger').addEventListener('click', event =>{
       // set text content to be burger typed into the input
       burgerElem.innerHTML = `
       ${document.getElementById('burger_name').value}
-       <button class="devour" data-id="${data.insertId}">Devour</button>
+      <button class="devour" data-id="${data.insertId}">Devour</button>
       `
       //append input onto burger list
       document.getElementById('userBurgerName').append(burgerElem)
