16c076200d747c7472b33c667e7aea6a0c847237
Erika <erika.paige.handley@gmail.com>
2020-05-29 23:02:44 -0700

    updated Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2')
 
 // connection w/ new mysql connection
-module.exports = mysql.createConnection('mysql://root:rootroot1@localhost:3306/burgers_db')
+// module.exports = mysql.createConnection('mysql://root:rootroot1@localhost:3306/burgers_db')
 
 // below is connection use once deployed on heroku / JAWS
-// module.exports = mysql.createConnection(process.env.JAWSDB_URL || process.env.LOCAL_URL)
+module.exports = mysql.createConnection(process.env.JAWSDB_URL || process.env.LOCAL_URL)
